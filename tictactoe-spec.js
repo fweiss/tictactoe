2bf985053df78a45343673317bebc65309d2c6d0
fweiss <GitHub$76>
2014-07-21 11:53:36 -0700

    Revise board status from 'game over' to 'winner' and 'draw'.

--- tictactoe-spec.js
+++ tictactoe-spec.js
@@ -3,24 +3,24 @@ describe('tictactoe', function() {
     beforeEach(function() {
         board = Board.newBoard();
     });
-    it('should get a game over when all spaces are filled', function() {
+    it('should get draw when all spaces are filled', function() {
         // must be a cat's game
         board.markAll('XOOOXXXXO');
         expect(board.status()).toEqual(Board.CATS_GAME);
     });
-    it('should get a game over when a row is filled with X\'s', function() {
+    it('should get winner X when a row is filled with X\'s', function() {
         board.markAll('XXX------');
         expect(board.status()).toEqual(Board.WINNER_X);
     });
-    it('should get a game over when a column is filled with O\'s', function() {
+    it('should get winner O when a column is filled with O\'s', function() {
         board.markAll('O--O--O--');
         expect(board.status()).toEqual(Board.WINNER_O);
     });
-    it('should get game over when left diagonal is filled with X\'s', function() {
+    it('should get winner X when left diagonal is filled with X\'s', function() {
         board.markAll('X---X---X');
         expect(board.status()).toEqual(Board.WINNER_X);
     });
-    it('should get game over when right diagonal is filled with O\'s', function() {
+    it('should get winner O when right diagonal is filled with O\'s', function() {
         board.markAll('--O-O-O--');
         expect(board.status()).toEqual(Board.WINNER_O);
     });
